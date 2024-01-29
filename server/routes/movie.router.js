d57d7cb6eedb40ca34bb4b715c1c96e4f36cfcca
slindsey9150 <stevie.lindsey.jr@gmail.com>
2024-01-29 11:34:47 -0600

    deleted various elements that I never got to work

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -19,17 +19,12 @@ router.get('/', (req, res) => {
 });
 router.get('/:id', (req, res) => {
   console.log("req.params.id", req.params.id)
-     //empty object to store details about the movie clicked
   let movieDetails = {}
   console.log("movie details - should be empty", movieDetails)
-     //SQL query to select all colums associated with the id (row)
   let queryText =
     `SELECT * FROM "movies"
   WHERE "id" = $1;`;
-
-      //array that contains the movie id called in the url path
   let queryParams = [req.params.id];
-
   pool.query(queryText, queryParams)
     .then(result => {
       movieDetails = { ...movieDetails, ...result.rows[0] }
@@ -56,53 +51,6 @@ router.get('/:id', (req, res) => {
 }) 
 
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  // RETURNING "id" will give us back the id of the created movie
-  const insertMovieQuery = `
-    INSERT INTO "movies" 
-      ("title", "poster", "description")
-      VALUES
-      ($1, $2, $3)
-      RETURNING "id";
-  `;
-  const insertMovieValues = [
-    req.body.title,
-    req.body.poster,
-    req.body.description
-  ]
-  // FIRST QUERY MAKES MOVIE
-  pool.query(insertMovieQuery, insertMovieValues)
-    .then(result => {
-      // ID IS HERE!
-      console.log('New Movie Id:', result.rows[0].id);
-      const createdMovieId = result.rows[0].id
 
-      // Now handle the genre reference:
-      const insertMovieGenreQuery = `
-        INSERT INTO "movies_genres" 
-          ("movie_id", "genre_id")
-          VALUES
-          ($1, $2);
-      `;
-      const insertMovieGenreValues = [
-        createdMovieId,
-        req.body.genre_id
-      ]
-      // SECOND QUERY ADDS GENRE FOR THAT NEW MOVIE
-      pool.query(insertMovieGenreQuery, insertMovieGenreValues)
-        .then(result => {
-          //Now that both are done, send back success!
-          res.sendStatus(201);
-        }).catch(err => {
-          // catch for second query
-          console.log(err);
-          res.sendStatus(500)
-      })
-    }).catch(err => { // 👈 Catch for first query
-      console.log(err);
-      res.sendStatus(500)
-    })
-})
 
 module.exports = router;
